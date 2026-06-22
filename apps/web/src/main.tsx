d0e0863e49a6833d9cbd381993c3cdc921a1ac47
otiseduncan <otiseduncan@gmail.com>
2026-06-22 01:05:54 -0400

    feat(web): route cockpit path to standalone surface

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
+import { CockpitWindow } from './app/CockpitWindow';
 import './styles.css';
+
+const RootApp = window.location.pathname.startsWith('/cockpit') ? CockpitWindow : App;
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>
 );
